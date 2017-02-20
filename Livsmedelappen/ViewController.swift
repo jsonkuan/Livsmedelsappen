cb6c70139b559580aea2107924a6d1fbb07814f1
jas0n- <jsonkuan@gmail.com>
2017-02-20 16:18:12 +0100

    Initial View Layout with sample data

--- Livsmedelappen/ViewController.swift
+++ Livsmedelappen/ViewController.swift
@@ -8,18 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func displayResult(_ sender: UIButton) {
+    
     }
-
-
 }
+    
 
