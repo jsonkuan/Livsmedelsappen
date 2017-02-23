2eb4d0ba857ba8dbda12bfb790af35f6ea564032
jas0n- <jsonkuan@gmail.com>
2017-02-23 11:02:03 +0100

    Implemented UISearchController & UISearchResultsUpdating Protocol

---
+++ Livsmedelappen/MyTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  MyTableViewCell.swift
+//  Livsmedelappen
+//
+//  Created by Jason Kuan on 2017-02-23.
+//  Copyright © 2017 jsonkuan. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    var foodProduct: String?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Livsmedelappen/SearchListTableViewController.swift
+++ Livsmedelappen/SearchListTableViewController.swift
@@ -8,82 +8,107 @@
 
 import UIKit
 
-class SearchListTableViewController: UITableViewController {
+class SearchListTableViewController: UITableViewController, UISearchResultsUpdating {
     
-    var sampleArray = ["One", "Two", "Three"]
-    var sampleDetailText = ["20 calories", "30 calories", "40 calories"]
+    var sampleData = ["one", "two", "three", "four", "twohundred"]
+    var searchController: UISearchController!
+    var searchResult: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
+        
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+//        searchController.dimsBackgroundDuringPresentation = false
+        tableView.tableHeaderView = searchController.searchBar
+        definesPresentationContext = true
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return sampleArray.count
+        return (shouldUseSearchResult ? searchResult : sampleData).count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-        cell.textLabel?.text = sampleArray[indexPath.item]
-        cell.detailTextLabel?.text = sampleDetailText[indexPath.item]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as! MyTableViewCell
+        if shouldUseSearchResult {
+            cell.textLabel?.text = searchResult[indexPath.row]
+            cell.foodProduct = searchResult[indexPath.row]
+        } else {
+            cell.textLabel?.text = sampleData[indexPath.row]
+            cell.foodProduct = sampleData[indexPath.row]
+        }
         cell.imageView?.image = #imageLiteral(resourceName: "logo")
-        
         
         return cell
     }
-
-
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+
+        // MARK: - UISearchResultsUpdating protocol methods
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        if let text = searchController.searchBar.text?.lowercased() {
+            searchResult = sampleData.filter { $0.contains(text) }
+        } else {
+            searchResult = []
+        }
+        tableView.reloadData()
     }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    var shouldUseSearchResult: Bool {
+        if let text = searchController.searchBar.text {
+            if text.isEmpty {
+                return false
+            }
+        }
+        return searchController.isActive
     }
-    */
-
+    
+         // MARK: - Navigation
+    
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let cell = sender as? MyTableViewCell {
+            segue.destination.title = cell.foodProduct
+        }
+     }
+    
 }
